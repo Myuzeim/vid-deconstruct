0e55e911558404279d9b082fbf2ad765e5b6451a
Myuzeim <myuzeim@gmail.com>
2025-08-10 09:51:21 -0700

    move image loading to another thread

---
+++ iframe_model.py
@@ -0,0 +1,81 @@
+from PyQt6.QtCore import QAbstractListModel, QFile, QModelIndex, Qt
+from PyQt6.QtCore import QByteArray, pyqtSignal, QThread, QMutex, QMutexLocker
+from PyQt6.QtGui import QPixmap, QPixmapCache
+import sys
+import subprocess
+import ffmpeg
+
+class FrameLoader(QThread):
+    added = pyqtSignal(int)
+
+    def __init__(self, timestamps: list[str], load: dict[int,QByteArray], file: QFile):
+        super().__init__()
+        self.load = load
+        self.timestamps = timestamps
+        self.file = file
+    
+    def run(self):
+        for i, time in enumerate(self.timestamps):
+            fpix = ffmpeg.input(self.file.fileName(), ss=time) \
+                            .filter('scale',128,128) \
+                            .output('pipe:1', vframes=1, format='image2', vcodec='mjpeg', qscale=5) \
+                            .global_args('-hide_banner', '-loglevel', 'error') \
+                            .run(capture_stdout=True)
+            
+            self.load[i] = QByteArray(fpix[0])
+            self.added.emit(i)
+        
+
+class IFrameModel(QAbstractListModel):
+    def __init__(self, file: QFile):
+        super().__init__()
+        self.file = file
+        #process video
+        cmd = ["ffprobe", 
+               "-select_streams", "v:0", 
+               "-show_frames", 
+               "-skip_frame", "nokey", 
+               "-show_entries", "frame=pts_time",
+               "-of", "csv=p=0",
+               self.file.fileName()]
+        
+        try:
+            csv_frames = subprocess.run(cmd, capture_output=True, text=True, check=True)
+        except subprocess.CalledProcessError as e:
+            print("reason: %s" % e.stderr.strip())
+            sys.exit()
+
+        self.iframe_timestamps = [line.split(",")[0] for line in csv_frames.stdout.strip().splitlines()]
+        self.frame_load = dict[int,QByteArray]()
+        self.frame_loader = FrameLoader(self.iframe_timestamps,self.frame_load,self.file)
+        self.frame_loader.added.connect(self.frame_added)
+        self.frame_loader.start()
+
+    def frame_added(self, i: int):
+        idx = self.index(i)
+        self.dataChanged.emit(idx, idx, [Qt.ItemDataRole.DecorationRole])
+
+    def rowCount(self, parent=None):
+        return len(self.iframe_timestamps)
+    
+    def columnCount(self, parent = None):
+        return 2
+    
+    def data(self, index: QModelIndex, role=Qt.ItemDataRole.DisplayRole):
+        if not index.isValid():
+            return None
+        if role == Qt.ItemDataRole.DisplayRole:
+            return self.iframe_timestamps[index.row()]
+        if role == Qt.ItemDataRole.DecorationRole:
+            if index.row() in self.frame_load:
+                pix = QPixmapCache.find(str(index.row()))
+                if pix:
+                    return pix
+                pix = QPixmap()
+                pix.loadFromData(self.frame_load[index.row()], "JPG")
+                QPixmapCache.insert(str(index.row()),pix)
+            else:
+                return QPixmap(128,128)
+        return None
+
+            
--- vd_main.py
+++ vd_main.py
@@ -1,57 +1,10 @@
-from PyQt6 import QtCore, QtWidgets, QtGui
+from PyQt6.QtWidgets import QMainWindow, QListView, QApplication, QFileDialog
+from PyQt6.QtCore import QFile, QFileInfo
+from iframe_model import IFrameModel
 import sys, os
-import subprocess
-import ffmpeg
 
-QtGui.QPixmapCache.setCacheLimit(128*128*3*30)
-
-class FrameModel(QtCore.QAbstractListModel):
-    def __init__(self, file: QtCore.QFile):
-        super().__init__()
-        self.file = file
-        #process video
-        cmd = ["ffprobe", 
-               "-select_streams", "v:0", 
-               "-show_frames", 
-               "-skip_frame", "nokey", 
-               "-show_entries", "frame=pts_time",
-               "-of", "csv=p=0",
-               self.file.fileName()]
-        
-        try:
-            csv_frames = subprocess.run(cmd, capture_output=True, text=True, check=True)
-        except subprocess.CalledProcessError as e:
-            print("reason: %s" % e.stderr.strip())
-            sys.exit()
-
-        
-        self.iframe_timestamps = [line.split(",")[0] for line in csv_frames.stdout.strip().splitlines()]
-        
-
-    def rowCount(self, parent=None):
-        return len(self.iframe_timestamps)
-    def data(self, index: QtCore.QModelIndex, role=QtCore.Qt.ItemDataRole.DisplayRole):
-        if not index.isValid():
-            return None
-        if role == QtCore.Qt.ItemDataRole.DisplayRole:
-            return self.iframe_timestamps[index.row()]
-        if role == QtCore.Qt.ItemDataRole.DecorationRole:
-            frame = self.iframe_timestamps[index.row()]
-            pix = QtGui.QPixmapCache.find(str(frame))
-            if not pix:
-                pix = QtGui.QPixmap()
-                fpix = ffmpeg.input(self.file.fileName(), ss=frame) \
-                                    .filter('scale',128,-1) \
-                                    .output('pipe:1', vframes=1, format='image2', vcodec='png') \
-                                    .run(capture_stdout=True)
-                pix.loadFromData(QtCore.QByteArray(fpix[0]),"PNG")
-                QtGui.QPixmapCache.insert(str(frame), pix)
-            return pix
-
-
-
-class VDMainWindow(QtWidgets.QMainWindow):
-    def __init__(self, file: QtCore.QFile, parent=None):
+class VDMainWindow(QMainWindow):
+    def __init__(self, file: QFile, parent=None):
         super().__init__(parent)
         self.file = file
         withoutPath = os.path.split(self.file.fileName())[-1]
@@ -60,26 +13,23 @@ class VDMainWindow(QtWidgets.QMainWindow):
         
 
         # set up the window
-        fmodel = FrameModel(self.file)
-
-        ifListView = QtWidgets.QListView()
-        ifListView.setModel(fmodel)
-        
-        self.setCentralWidget(ifListView)
-
+        self.ifListView = QListView()
+        self.fmodel = IFrameModel(self.file)
+        self.ifListView.setModel(self.fmodel)
+        self.setCentralWidget(self.ifListView)
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     
     if(len(sys.argv) > 1):
         if not os.path.exists(sys.argv[1]):
             print("No such file")
-        file = QtCore.QFile(sys.argv[1])
-        if QtCore.QFileInfo(file).completeSuffix() != "mp4":
+        file = QFile(sys.argv[1])
+        if QFileInfo(file).completeSuffix() != "mp4":
             print("Invalid file")
 
     else:
-        fileUrl, _ = QtWidgets.QFileDialog.getOpenFileUrl(
+        fileUrl, _ = QFileDialog.getOpenFileUrl(
             caption="Select Video",
             filter=".mp4 (*.mp4)"
         )
@@ -87,7 +37,7 @@ if __name__ == "__main__":
             print("You must select a file, or provide one as an argument!")
             sys.exit()
         
-        file = QtCore.QFile(fileUrl.toString())
+        file = QFile(fileUrl.toString())
 
 
     mw = VDMainWindow(file)
